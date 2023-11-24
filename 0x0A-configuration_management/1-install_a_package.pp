ae46f81e5bbb63c2b9b14b450a58fa0ba077ff76
Emil dawood <edawood997@gmail.com>
2023-11-24 12:31:09 +0200

    d

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#install flask with pip3
+# install flask with pip3
 
-exec { 'install_flask_with_pip3':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
