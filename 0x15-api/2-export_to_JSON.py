421933593f40c8db72cee8f26f5bdd83b661b83a
Emil dawood <edawood997@gmail.com>
2024-01-02 17:31:47 +0200

    OKKK

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3=
 """gather_data_from_an_API"""
 import json
+import requests
+import sys
 if __name__ == "__main__":
-    import json
-    import requests
-    import sys
     num = int(sys.argv[1])
     url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
     fetch_api = requests.get(url)
