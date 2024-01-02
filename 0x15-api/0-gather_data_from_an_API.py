ca99ab92fe9d60a5737b4168ad69e93c12728525
Emil dawood <edawood997@gmail.com>
2024-01-02 14:06:15 +0200

    Done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """gather_data_from_an_API"""
-import json
-import requests
-import sys
-num = int(sys.argv[1])
-fetch_api = requests.get(f'https://jsonplaceholder.typicode.com/users/{num}')
-todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-list_of_tasks = []
-count = 0
-todos_dic = json.loads(todos.content)
-dic = json.loads(fetch_api.content)
-for todo in todos_dic:
-    if todo["userId"] == num:
-        if todo["completed"] is True:
-            list_of_tasks.append("\t{}".format(todo["title"]))
-            count += 1
-print("Employee {} is done with tasks({}/20):".format(dic["name"], count))
-for task in list_of_tasks:
-    print(task)
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+    num = int(sys.argv[1])
+    fetch_api = requests.get(f'https://jsonplaceholder.typicode.com/users/{num}')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    list_of_tasks = []
+    count = 0
+    todos_dic = json.loads(todos.content)
+    dic = json.loads(fetch_api.content)
+    for todo in todos_dic:
+        if todo["userId"] == num:
+            if todo["completed"] is True:
+                list_of_tasks.append("\t{}".format(todo["title"]))
+                count += 1
+    print("Employee {} is done with tasks({}/20):".format(dic["name"], count))
+    for task in list_of_tasks:
+        print(task)
