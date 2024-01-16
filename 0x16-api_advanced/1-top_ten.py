88a8b0cd9e8ccc398aee6660c0db754583c31e8e
Emil dawood <edawood997@gmail.com>
2024-01-16 13:58:27 +0200

    second task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,14 +3,18 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """get number of subscribers from api"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     h = {'User-Agent': 'MyPythonScript/1.0'}
     req = requests.get(url, headers=h)
     if req.status_code != 200:
         return 0
     if (req.json().get('data').get('subscribers') is not None):
-        return req.json().get('data').get('subscribers')
+        hot = req.json().get('data').get('children')
+        if hot is None:
+            return print(None)
+        for hotta in hot:
+            print(hotta.get('data'.get('title')))
     else:
-        return 0
+        return print(None)
