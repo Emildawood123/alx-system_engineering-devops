833f0ef4256d7739d7c448cb46cda471523fd2b7
Emil dawood <edawood997@gmail.com>
2024-01-16 13:31:13 +0200

    add header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """get number of subscribers from api"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    req = requests.get(url)
+    req = requests.get(url, headers={'User-Agent': 'MyPythonScript/1.0'})
     if req.status_code != 200:
         return 0
     try:
