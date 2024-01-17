812fd2fbc23508534209b37856f5a6b5cff0f6ed
Emil dawood <edawood997@gmail.com>
2024-01-17 18:13:20 +0200

    dddddd

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """get number of subscribers from api"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    h = {'User-Agent': 'Emil'}
+    h = {'User-Agent': 'cheetahExpert6025'}
     req = requests.get(url, headers=h)
     if req.status_code != 200:
         return 0
